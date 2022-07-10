9bea4d02ee5bda340ef1c0cf563f85488ee9ea3a
Steve Tatton <86009962+howsilence@users.noreply.github.com>
2022-07-10 11:35:12 -0400

    Update post_purchase.rb

--- lightweight_calls/post_purchase.rb
+++ lightweight_calls/post_purchase.rb
@@ -35,53 +35,7 @@ require 'recurly'
     puts "Created Charge Invoice #{invoice_collection.charge_invoice}"
     puts "Created Credit Invoices #{invoice_collection.credit_invoices}"
   rescue Recurly::Errors::ValidationError => e
-    # If the request was invalid, you may want to tell your user
-    # why. You can find the invalid params and reasons in e.recurly_error.params
     puts "ValidationError: #{e.recurly_error.params}"
   end
   
 
-    # purchase = {
-    #   currency: "USD",
-    #   account: {
-    #     code: "code-123987456",
-    #     first_name: "Ahsoka",
-    #     last_name: "Tano",
-    #     username: "LittleSoka",
-    #     email: "Snips501s@therepublic.com",
-    #   },
-    #   shipping:{
-    #    address:{
-    #     first_name: "Ahsoka",
-    #     last_name: "Tano",
-    #     street1: "1 Exiled Padawan",
-    #     city: "Phoenix",
-    #     postal_code: "11211",
-    #     country: "US",
-    #    },
-    #   },
-    #   line_items: [
-    #     {
-    #     currency: "USD",
-    #     unit_amount: 0,
-    #     type: "charge",
-    #     },
-    #   ],
-    #   subscriptions: [
-    #     { plan_code: "White-Sabers",
-    #      unit_amount: 0,
-    #     },
-    #   ],
-    # }
-    # invoice_collection = @client.create_purchase(
-    #   body: purchase
-    # )
-    # puts "Created Charge Invoice #{invoice_collection.charge_invoice}"
-    # puts "Created Credit Invoices #{invoice_collection.credit_invoices}"
-#   rescue Recurly::Errors::ValidationError => e
-#     # If the request was invalid, you may want to tell your user
-#     # why. You can find the invalid params and reasons in e.recurly_error.params
-#     puts "ValidationError: #{e.recurly_error.params}"
-      #line 19 after email billing_info: {
-        #   token_id: rjs_token_id
-        # },
